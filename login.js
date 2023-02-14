8d01e75cbb7ea333bcbf6335b6495402a4fed0f2
Jones <Jonessajan3000@gmail.com>
2023-02-14 10:26:22 +0530

    login functionality added

---
+++ login.js
@@ -0,0 +1,33 @@
+var form = document.getElementById("addForm");
+form.addEventListener("submit", login);
+
+async function login(e) {
+  e.preventDefault();
+  
+  try { 
+    var newItem1 = e.target.email.value;
+    var newItem2 = e.target.password.value;
+    const data = {
+      email: newItem1,
+      password: newItem2,
+    };
+
+
+    const response = await axios.post(
+      "http://localhost:3000/user/loginuser",
+      data
+    );
+    console.log(response.data);
+
+    document.getElementById('message').innerText=response.data;
+
+    document.getElementById("item1").value = "";
+    document.getElementById("item2").value = "";
+
+
+  } catch (error) {
+    console.error(error);    
+    document.getElementById('message').innerText=error;
+
+  }
+}
